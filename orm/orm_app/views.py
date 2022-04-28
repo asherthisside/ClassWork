3b81891419480100e919b3f2b5dab8ea3edc69fb
Asher <haarisharry007@gmail.com>
2022-04-28 13:31:22 +0530

    Updated orm

--- orm/orm_app/views.py
+++ orm/orm_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def index(request):
+    kids = Kid.objects.all()
+    context = {'all_kids' : kids}
+    return render(request, 'index.html', context)
